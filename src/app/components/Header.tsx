8fee12ddee86019c5e10430f017b257443a35b44
Chakkapong Max <chakkaphong.pr@vulcan.bluebik.com>
2025-05-17 14:34:13 +0700

    Revert "Reapply "Reapply "test"""
    This reverts commit c0e54ee9cb12a2e630f2503555c275caad170fc5.

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,8 +1,6 @@
 "use client"
 import Image from "next/image"
 import { useEffect, useState } from "react";
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import { Badge, Box, Button, IconButton, Popover } from "@mui/material";
 
 const newDateWithTimezone = () => new Date().toLocaleString(
     "en-US", 
@@ -20,27 +18,7 @@ const newDateWithTimezone = () => new Date().toLocaleString(
 );
 
 export default function Header() {
-    const [notiCount] = useState([
-        {
-            notiDetail: "This is Notification 1",
-        },
-        {
-            notiDetail: "This is Notification 2",
-        }
-    ]);
     const [date, setDate] = useState(newDateWithTimezone());
-    const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
-
-    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-        setAnchorEl(event.currentTarget);
-    };
-
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
-
-    const open = Boolean(anchorEl);
-    const id = open ? 'simple-popover' : undefined;
     
     useEffect(() => {
         const intervalId = setInterval(() => {
@@ -59,29 +37,6 @@ export default function Header() {
             </div>
             <div className="date-time">
                 {date.toString()}
-                <IconButton onClick={handleClick}>
-                    <Badge badgeContent={notiCount.length} color="warning">
-                        <NotificationsIcon />
-                    </Badge>
-                </IconButton>
-                <Popover
-                    id={id}
-                    open={open}
-                    anchorEl={anchorEl}
-                    onClose={handleClose}
-                    anchorOrigin={{
-                        vertical: 'bottom',
-                        horizontal: 'left',
-                    }}
-                >
-                    {notiCount.map((noti, index) => {
-                        return (
-                            <Box key={noti.notiDetail + index} >
-                                <Button sx={{ p: 2 }}>This is Notification {noti.notiDetail}</Button>
-                            </Box>
-                        )
-                    })}
-                </Popover>
             </div>
         </div>
     ) 
